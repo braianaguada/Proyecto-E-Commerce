146179a2eeddf7490ab82d6771b0590c52ed3fa5
Braian Aguada <braianaguada@gmail.com>
2022-09-29 15:30:07 -0300

    Update

--- CLIENT/src/components/NavBar.jsx
+++ CLIENT/src/components/NavBar.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './NavBar.module.css';
+import cart from '../media/cart.svg'
 
 export default function NavBar() {
   return (
     <div className={styles.NavBar}>
-      <div className={styles.cart}>
-      </div>
+      <img className={styles.cart} src={cart} alt="icon"/>
       <ul>
-        <li>cart</li>
         <li>about</li>
         <li>contact</li>
       </ul>
--- CLIENT/src/components/Products.jsx
+++ CLIENT/src/components/Products.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from './Products.module.css'
 import heart from '../media/heart.svg'
+import sum from '../media/sum.svg'
+import circle from '../media/circle.svg'
+import circleinverted from '../media/circleinverted.svg'
+import stars from '../media/stars.svg'
 
 export default function Products({ id, title, image, price, description }) {
   return (
     <div className={styles.cards}>
-      <img src={image} alt="product" />
+      <img className={styles.image} src={image} alt="product" />
       <div className={styles.info}>
       <Link className={styles.link} to={`/Details/${id}`}>
         <h1>{title}</h1>
       </Link>
-      <h2>{price}</h2>
+      <h2>${price}</h2>
       <div className={styles.iconsContainer}>
       <img className={styles.heart} src={heart} alt="icon"/>
+      <img className={styles.sum} src={sum} alt="icon"/>
+      <img className={styles.circle} src={circle} alt="icon"/>
+      <img className={styles.circleinverted} src={circleinverted} alt="icon"/>
+      <img className={styles.stars} src={stars} alt="icon"/>
       </div>
       </div>
     </div>
